21cb9833197158b8dc470f3d23165143920bf8a9
Tobias Rohner <torohner@student.ethz.ch>
2021-07-13 15:12:29 +0200

    Fixed postprocessing of taylor_vortex

--- experiments/TaylorVortex2D/postprocessing.py
+++ experiments/TaylorVortex2D/postprocessing.py
@@ -9,16 +9,16 @@ import sys
 def analytic_sol(N, t):
     x = np.linspace(-8, 8, N, False)
     y = np.linspace(-8, 8, N, False)
-    xm, ym = np.meshgrid(x, y)
-    u = -ym * np.exp(0.5 * (1 - (xm - 4*t)**2 - ym**2)) + 8
-    v = (xm - 4*t) * np.exp(0.5 * (1 - (xm - 4*t)**2 - ym**2))
+    ym, xm = np.meshgrid(x, y)
+    u = -ym * np.exp(0.5 * (1 - (xm - 8*t)**2 - ym**2)) + 8
+    v = (xm - 8*t) * np.exp(0.5 * (1 - (xm - 8*t)**2 - ym**2))
     return u/16, v/16
 
 
 def read_sol(N):
-    with nc.Dataset('taylor_vortex_N{}_T1.0.nc'.format(N)) as f:
-        u = f['sample_0_time_1.000000_u'][:]
-        v = f['sample_0_time_1.000000_v'][:]
+    with nc.Dataset('taylor_vortex_N{}_T0.5.nc'.format(N)) as f:
+        u = f['sample_0_time_0.500000_u'][:]
+        v = f['sample_0_time_0.500000_v'][:]
     return u, v
 
 
@@ -30,21 +30,21 @@ def pad_fourier(u_hat, N_pad):
     u_pad_hat[:Nf,-Nf:] = u_hat[:Nf,-Nf:]
     u_pad_hat[-Nf:,:Nf] = u_hat[-Nf:,:Nf]
     u_pad_hat[-Nf:,-Nf:] = u_hat[-Nf:,-Nf:]
-    return u_pad_hat
+    return (N_pad / N)**2 * u_pad_hat
 
 
 def compute_err(N, N_ref):
     u, v = read_sol(N)
-    u_ref, v_ref = analytic_sol(N_ref, 1)
+    u_ref, v_ref = analytic_sol(N_ref, 0.5)
     u_hat = np.fft.fft2(u)
     v_hat = np.fft.fft2(v)
     u_pad_hat = pad_fourier(u_hat, N_ref)
     v_pad_hat = pad_fourier(v_hat, N_ref)
-    u_pad = np.fft.ifft2(u_pad_hat)
-    v_pad = np.fft.ifft2(v_pad_hat)
+    u_pad = np.real(np.fft.ifft2(u_pad_hat))
+    v_pad = np.real(np.fft.ifft2(v_pad_hat))
     err2_u = np.sum(np.abs(u_ref - u_pad)**2)
     err2_v = np.sum(np.abs(v_ref - v_pad)**2)
-    err = np.sqrt(err2_u + err2_v)
+    err = np.sqrt(err2_u + err2_v) / N_ref**2
     return err
 
 
