763cfab30a994698e747d5d9638c4f58c4d8db3a
Tobias Rohner <torohner@student.ethz.ch>
2021-10-13 15:22:23 +0200

    Licensing

--- include/azeban/cuda/operations/cufft_mpi.hpp
+++ include/azeban/cuda/operations/cufft_mpi.hpp
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of azeban (https://github.com/TobiasRohner/azeban).
+ * Copyright (c) 2021 Tobias Rohner.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef CUFFT_MPI_H_
 #define CUFFT_MPI_H_
 
