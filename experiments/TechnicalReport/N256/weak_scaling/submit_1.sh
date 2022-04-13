bd8b264cdda7740390322322c6d2126a89815a25
Tobias Rohner <tobias.rohner@math.ethz.ch>
2022-04-13 16:36:27 +0200

    Extend experiments

--- experiments/TechnicalReport/N256/weak_scaling/submit_1.sh
+++ experiments/TechnicalReport/N256/weak_scaling/submit_1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=tg_N256_weak_1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tobias.rohner@math.ethz.ch
 #SBATCH --account="s1069"
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
@@ -9,8 +11,8 @@
 #SBATCH --partition=normal
 #SBATCH --constraint=gpu
 #SBATCH --hint=nomultithread
-#SBATCH --switches=1
+#SBATCH --output=slurm-%x.%j.out
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-srun /users/trohner/azeban/build_profile/azeban /users/trohner/azeban/experiments/TechnicalReport/N256/weak_scaling/config.json
+srun /users/trohner/azeban/build_profile/azeban --ranks-per-sample=1 /users/trohner/azeban/experiments/TechnicalReport/N256/weak_scaling/config_1.json
---
+++ experiments/TechnicalReport/N256/weak_scaling/submit_8.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH --job-name=tg_N256_weak_8
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tobias.rohner@math.ethz.ch
+#SBATCH --account="s1069"
+#SBATCH --time=24:00:00
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+#SBATCH --output=slurm-%x.%j.out
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+srun /users/trohner/azeban/build_profile/azeban --ranks-per-sample=1 /users/trohner/azeban/experiments/TechnicalReport/N256/weak_scaling/config_8.json
