2f5c8c7bd82e06b4fc2fa1f3cc47caea3cc39627
Tobias Rohner <tobias.rohner@math.ethz.ch>
2022-04-29 14:31:16 +0200

    Ran all single sample strong scaling experiments

--- experiments/TechnicalReport/N512/submit.sh
+++ experiments/TechnicalReport/N512/submit.sh
@@ -3,8 +3,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tobias.rohner@math.ethz.ch
 #SBATCH --account="s1069"
-#SBATCH --time=24:00:00
-#SBATCH --nodes=4
+#SBATCH --time=03:00:00
+#SBATCH --nodes=8
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
@@ -15,4 +15,4 @@
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-srun /users/trohner/azeban/build_profile/azeban --ranks-per-sample=4 /users/trohner/azeban/experiments/TechnicalReport/N512/config.json
+srun /users/trohner/azeban/build_profile/azeban --ranks-per-sample=8 /users/trohner/azeban/experiments/TechnicalReport/N512/config.json
